92dc1036d44831e5be9921d94d55bbd604ac48c9
kevinsherrell <k.sherrell.1028@gmail.com>
2022-11-29 20:04:02 -0600

    cleaned up code in user route

--- routes/users.js
+++ routes/users.js
@@ -9,125 +9,148 @@ const {validateUser} = require('../validation/user');
 const jwt = require('jsonwebtoken');
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-  res.send('respond with a resource');
+    res.send('respond with a resource');
 });
 router.get('/message', (req, res) => {
-  const token = req.header(process.env.TOKEN_HEADER_KEY);
-  console.log(token);
-  const verified = jwt.verify(token, process.env.JWT_SECRET_KEY);
-  if (verified === null) {
+    const token = req.header(process.env.TOKEN_HEADER_KEY);
+    console.log(token);
+    const verified = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    if (verified === null) {
+        res.json({
+            success: false,
+            message: 'ID token could not be verified'
+        })
+    }
+
     res.json({
-      success: false,
-      message: 'ID token could not be verified'
+        success: true,
+        message: "Token verified"
     })
-  }
-  if (verified && verified.userData.scope === 'user') {
-    res.json({
-      success: true,
-      message: "I am a user"
-    });
-  } else if (verified && verified.userData.scope === 'admin') {
-    res.json({
-      success: true,
-      message: "I am an admin"
-    })
-  }
-  console.log(verified);
+
+    // if (verified && verified.userData.scope === 'user') {
+    //     res.json({
+    //         success: true,
+    //         message: "I am a user"
+    //     });
+    // } else if (verified && verified.userData.scope === 'admin') {
+    //     res.json({
+    //         success: true,
+    //         message: "I am an admin"
+    //     })
+    // }
+
 })
 /* POST create user*/
 router.post('/register', async (req, res) => {
-  console.log("create has been called");
-  let hashed;
-  const userObj = {
-    email: req.body.email,
-    password: req.body.password,
-    verify: req.body.verify
-  }
-  // validate that user object is correct
-  console.log("user object", userObj);
-  // console.log("req.body", req.body);
-  const validated = validateUser(userObj);
-  //
-  try {
-    const userFound = await db().collection('users').findOne({email: userObj.email});
-    if (userFound !== null) {
-      return res.json({
-        success: false,
-        message: 'this email exists'
-      })
+    console.log("/register");
+    let hashed;
+    const userObj = {
+        email: req.body.email,
+        password: req.body.password,
+        verify: req.body.verify
     }
+    // validate that user object is correct
+    console.log("user object", userObj);
+    // console.log("req.body", req.body);
+    const validated = validateUser(userObj);
+    //
+    try {
 
-    if (validated.isValid) {
-      hashed = await bcrypt.hash(userObj.password, saltRounds);
-      userObj.password = hashed;
-    } else {
-      throw new Error("error");
-    }
-    console.log("req.body",req.body);
-
-    db().collection('users').insertOne(
-        {
-          _id: uuid(),
-          email: userObj.email,
-          password: userObj.password
+        const userFound = await db().collection('users').findOne({email: userObj.email});
+        if (userFound !== null) {
+            return res.json({
+                success: false,
+                message: 'Email already exists, please try another email'
+            })
         }
-    ).then(result => {
-      res.json({
-        success: true,
-        result: result
-      })
-    })
-  } catch (error) {
-    console.log(error);
-    res.json({
-      success: false,
-      isValid: validated.isValid,
-      errors: validated.errors
-    })
-  }
+
+        if (validated.isValid) {
+            hashed = await bcrypt.hash(userObj.password, saltRounds);
+            userObj.password = hashed;
+        }
+        // else {
+        //     throw new Error("error");
+        // }
+
+        db().collection('users').insertOne(
+            {
+                _id: uuid(),
+                email: userObj.email,
+                password: userObj.password
+            }
+        ).then(result => {
+            res.json({
+                success: true,
+                result: result
+            })
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.json({
+            success: false,
+            isValid: validated.isValid,
+            errors: validated.errors
+        })
+    }
 
 
 })
 
 /* POST login user*/
 router.post('/login', async (req, res) => {
+    console.log("/login");
 
-  const secretKey = process.env.JWT_SECRET_KEY;
-  console.log("loggin running");
-  const foundUser = await db().collection('users').findOne({email: req.body.email});
-  const userData = {
-    date: new Date(),
-    id: foundUser._id,
-    scope: foundUser.email.includes("codeimmersives.com") ? 'admin' : 'user'
-  }
-  const payload = {
-    userData: userData,
-    exp: Math.floor(Date.now() / 1000) + (60 * 60)
-  };
+    const errors = [];
 
-  bcrypt.compare(req.body.password, foundUser.password)
-      .then(result => {
-        if (result === true) {
-          const token = jwt.sign(payload, secretKey);
+    const secretKey = process.env.JWT_SECRET_KEY;
+    const foundUser = await db().collection('users').findOne({email: req.body.email});
 
-
-          res.json({
-            success: true,
-            token: token,
-            email: foundUser.email
-          })
-        } else {
-          res.json({
-            success: false
-          })
-        }
-      })
-      .catch(err => {
-        res.send({
-          success: false,
-          message: err
+    if (foundUser === null) {
+        errors.push({
+            type: "user",
+            message: "This email does not exist"
         })
-      })
+    }
+
+    const userData = {
+        date: new Date(),
+        id: foundUser._id,
+        // scope: foundUser.email.includes("codeimmersives.com") ? 'admin' : 'user'
+    }
+
+    const payload = {
+        userData: userData,
+        exp: Math.floor(Date.now() / 1000) + (60 * 60)
+    };
+    try {
+        bcrypt.compare(req.body.password, foundUser.password)
+            .then(result => {
+                if (result === true) {
+                    const token = jwt.sign(payload, secretKey);
+                    return res.json({
+                        success: true,
+                        token: token,
+                        email: foundUser.email
+                    })
+                } else {
+                    errors.push({
+                        type: 'user',
+                        message: "email or password invalid"
+                    })
+                    return res.json({
+                        success: false,
+                        errors: errors
+                    })
+                }
+            })
+    } catch (err) {
+        res.send({
+            success: false,
+            message: err
+        })
+    }
+
 })
 
 module.exports = router;
